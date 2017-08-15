b561a2bf683d0d045ce638d8ffc3e5821069cce2
miclefengzss <1280054628@qq.com>
2017-08-15 15:19:58 +0800

    create pagination

--- application/library/Database/DB.php
+++ application/library/Database/DB.php
@@ -25,13 +25,11 @@ class Database_DB
             return self::$_handler[$alias];
         }
 
-        if(!empty($this->_driver)){
+        if(empty($this->_driver)){
             $this->_set_driver($db_conf['driver']);
         }
 
-
-
-        var_dump($this->_driver,$db_conf,$db_conf['driver'],2222);
+        var_dump($this->_driver);
 
         $db = new $this->_driver();
 
