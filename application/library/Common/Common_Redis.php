1abfcd18d57cb2abdc1e0370728dc9f1ac8e2839
miclefengzss <1280054628@qq.com>
2017-08-21 10:32:18 +0800

    redis common connect

--- application/library/Common/Common_Redis.php
+++ application/library/Common/Common_Redis.php
@@ -22,7 +22,7 @@ class Common_Redis
 
     protected function __construct()
     {
-        if(!extension_loaded('redis')){
+        if (!extension_loaded('redis')) {
             throw new Exception("redis is not exist!");
         }
     }
@@ -33,12 +33,12 @@ class Common_Redis
         (is_null(self::$_instance)) && self::$_instance = new self();
 
         // basic parameter chk
-        if (($args_num = func_num_args()) === 1 && is_array($param = func_get_arg(0))){
+        if (($args_num = func_num_args()) === 1 && is_array($param = func_get_arg(0))) {
             self::$_instance->_conConf = $param;
-        }else{
-            if(func_num_args() === 0 || is_null(func_get_arg(0))){
-                self::$_instance->_conConf  = self::$_defaultconf;
-            }else{
+        } else {
+            if (func_num_args() === 0 || is_null(func_get_arg(0))) {
+                self::$_instance->_conConf = self::$_defaultconf;
+            } else {
                 throw new Exception("incorrect parameter(s)!");
             }
         }
@@ -48,35 +48,35 @@ class Common_Redis
 
     private function _link()
     {
-        if(!isset($this->_conConf['host']) || empty($this->_conConf['host'])){
+        if (!isset($this->_conConf['host']) || empty($this->_conConf['host'])) {
             $this->_conConf['host'] = self::$_defaultconf['host'];
         }
 
-        if(!isset($this->_conConf['port']) || empty($this->_conConf['port'])){
+        if (!isset($this->_conConf['port']) || empty($this->_conConf['port'])) {
             $this->_conConf['port'] = self::$_defaultconf['port'];
         }
 
-        if(!isset($this->_conConf['db']) || !is_numeric($this->_conConf['db'])){
+        if (!isset($this->_conConf['db']) || !is_numeric($this->_conConf['db'])) {
             $this->_conConf['db'] = self::$_defaultconf['db'];
         }
 
-        if(!isset($this->_conConf['persistent']) || !is_numeric($this->_conConf['persistent'])){
+        if (!isset($this->_conConf['persistent']) || !is_numeric($this->_conConf['persistent'])) {
             $this->_conConf['persistent'] = self::$_defaultconf['persistent'];
         }
 
-        if(!isset($this->_conConf['timeout']) || empty($this->_conConf['timeout'])){
+        if (!isset($this->_conConf['timeout']) || empty($this->_conConf['timeout'])) {
             $this->_conConf['timeout'] = 10;
         }
 
-        $k = implode("_",array($this->_conConf['host'],$this->_conConf['port'],$this->_conConf['db'],$this->_conConf['db'],$this->_conConf['persistent']));
+        $k = implode("_", array($this->_conConf['host'], $this->_conConf['port'], $this->_conConf['db'], $this->_conConf['db'], $this->_conConf['persistent']));
 
-        if(!isset(self::$_redisHandle[$k]) || get_resource_type(self::$_redisHandle[$k]) !== 'redis'){
+        if (!isset(self::$_redisHandle[$k]) || get_resource_type(self::$_redisHandle[$k]) !== 'redis') {
             $redis = new \Redis();
 
-            if(1 == $this->_conConf['persistent']){
-                $redis->pconnect($this->_conConf['host'],$this->_conConf['port'],$this->_conConf['timeout']);
-            }else{
-                $redis->connect($this->_conConf['host'],$this->_conConf['port'],$this->_conConf['timeout']);
+            if (1 == $this->_conConf['persistent']) {
+                $redis->pconnect($this->_conConf['host'], $this->_conConf['port'], $this->_conConf['timeout']);
+            } else {
+                $redis->connect($this->_conConf['host'], $this->_conConf['port'], $this->_conConf['timeout']);
             }
 
             $redis->select($this->_conConf['db']);
