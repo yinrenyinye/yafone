1758ae84dcfd7f68e5e4ed6bd9dd5b6e46fa4fbe
miclefengzss <1280054628@qq.com>
2017-08-28 12:06:46 +0800

    添加 Vendor / spout

--- application/controllers/Comment.php
+++ application/controllers/Comment.php
@@ -70,11 +70,14 @@ class CommentController extends Yaf_Controller_Abstract {
     public function xlsxAction()
     {
         $newFilePath = "../command/comment.xlsx";
+
         if(!file_exists($newFilePath)){
             touch($newFilePath);
         }
+
         $comment = new CommentModel();
         $data = $comment->get_list(1000);
+        var_dump(1111);
         $writer = WriterFactory::create(Type::XLSX);
         $writer->openToFile($newFilePath);
         $writer->addRows($data);
