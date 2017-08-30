9c81a5958264aedda2040d464ba2d1b8d4d900a7
miclefengzss <1280054628@qq.com>
2017-08-30 16:15:56 +0800

    test  lib/curl

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -6,7 +6,7 @@
  * @desc 默认控制器
  * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
  */
-use \Curl\Curl;
+use Curl\Curl;
 
 class IndexController extends Yaf_Controller_Abstract
 {
