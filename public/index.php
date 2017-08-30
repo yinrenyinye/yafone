c61cac6fa5b557f73b1f1b77e08166b696964d14
miclefengzss <1280054628@qq.com>
2017-08-30 17:37:23 +0800

    add index view

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 define('APPLICATION_PATH', realpath(dirname(__FILE__)."/../"));
-define('STATICS_PATH','/'.__DIR__.'/statics/backend');
+define('STATICS_PATH','/public/statics/backend');
 
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
 
