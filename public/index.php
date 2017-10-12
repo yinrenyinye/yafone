3f8ac7277024cc7649e708b466061f949088c7d4
zhangshangshang <zhangshangshang@modian.com>
2017-10-12 17:09:07 +0800

    reset orm fields

--- public/index.php
+++ public/index.php
@@ -10,12 +10,11 @@ $application->bootstrap()->run();
 
 // 结束收集
 $xhprof_data = xhprof_disable();
-
 $XHPROF_ROOT = "../xhprof";
 include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
 include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
-
 $xhprof_runs = new XHProfRuns_Default();
+
 // 将此次收集的性能数据存放到xhprof.output_dir目录
 $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_yaf");
 ?>
